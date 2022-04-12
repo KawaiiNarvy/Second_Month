6003d966a65cc4de818dd5ed7f04faf31dfc8666
Elina <elinabeishenalieva.eu@gmail.com>
2022-04-12 14:03:14 +0600

    second hw

--- HW_lesson2/ex1/main.js
+++ HW_lesson2/ex1/main.js
@@ -1,3 +1,10 @@
 const a = "*"
 for (let i = a; i.length < 7; i += "*")  
 console.log(i);
+
+// через while
+// let a = ""
+// while (a.length < 7){
+//     a += "*"
+//     console.log(a);
+// }
--- HW_lesson2/ex2/main.js
+++ HW_lesson2/ex2/main.js
@@ -5,3 +5,12 @@ for (let i = a; i < 101; i++){
     else if (i % 5 == 0) console.log( i + " 'Buzz'");
     else console.log(i);
 }
+// через while 
+// let a =1
+// while( a <= 100){
+//     if (a % 15 == 0) console.log(a + " fizzbuzz");
+//     else if (a % 3 == 0) console.log(a + " fizz");
+//     else if (a % 5 == 0) console.log(a + " buzz");
+//     else console.log(a);
+//     a++
+// }

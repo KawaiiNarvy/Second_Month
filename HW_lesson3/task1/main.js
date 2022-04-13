ded92d5d3dd5bb45e3e885ab1dc28385b1b069b5
Elina <elinabeishenalieva.eu@gmail.com>
2022-04-13 19:19:35 +0600

    third_hw

---
+++ HW_lesson3/task1/main.js
@@ -0,0 +1,11 @@
+function nums(num, num2) {
+    if (num < num2){
+         console.log(num + "<" + num2);
+     } else{
+         console.log(num + ">" + num2);
+     }
+ }
+ const num = 6
+ const num2 = 4
+ 
+ nums(num, num2)

c928ac6a47a4a6e623917ce32f3830a6b71aff83
Elina <elinabeishenalieva.eu@gmail.com>
2022-04-20 11:01:50 +0600

    changed fourth hw

--- HW_lesson4/main.js
+++ HW_lesson4/main.js
@@ -1,18 +1,29 @@
 let number = document.getElementById('num')
 let buttonPlus = document.getElementById('plus')
 let buttonMinus = document.getElementById('minus')
+let reset = document.getElementById('reset')
 
 let num = 0
 
 buttonPlus.onclick = () => {
     num ++
     count()
+    number.style.color = '#c7fcff'
 }
 buttonMinus.onclick = () => {
     num--
+    if (num < 1) {
+        num = 0
+    }
     count()
-}
+    number.style.color = '#d3ccff'
 
+}
+reset.onclick = () => {
+    num = 0
+    count()
+    number.style.color = '#fff'
+}
 function count(){
     number.innerHTML = num;
 }

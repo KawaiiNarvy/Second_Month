58302d524c72fa090b241bc9ddc300d199f3155e
Elina <elinabeishenalieva.eu@gmail.com>
2022-05-04 17:47:16 +0600

    HW^ (full)

--- HW_lesson6/ex3_HelloJack/main.js
+++ HW_lesson6/ex3_HelloJack/main.js
@@ -2,7 +2,6 @@ const inp = document.getElementById('input')
 const con = document.getElementById('container')
 const btn = document.getElementById('button')
 
-btn.addEventListener('click', function () {
-    if (inp.value.length) 
+btn.addEventListener('click', function () { 
     con.innerHTML = `Привет ${inp.value}`;
 })
